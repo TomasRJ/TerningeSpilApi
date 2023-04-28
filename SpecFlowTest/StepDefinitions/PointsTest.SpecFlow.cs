e2345add770dda9a984b0d0b422bdae5bf181299
Tomas R J <49104467+TomasRJ@users.noreply.github.com>
2023-04-28 16:21:33 +0200

    Flyttede vores Die model til en Model mappe

--- SpecFlowTest/StepDefinitions/PointsTest.SpecFlow.cs
+++ SpecFlowTest/StepDefinitions/PointsTest.SpecFlow.cs
@@ -4,9 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TerningeSpilApi;
 using TerningeSpilApi.Controllers;
-
+using TerningeSpilApi.Model;
 
 namespace SpecFlowTest.StepDefinitions
 
